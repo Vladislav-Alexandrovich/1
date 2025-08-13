f2faca9c2e91c9e2f27b78edc641a0734ee82263
Vladislav-Alexandrovich <vladislav.shakhov@gmail.com>
2025-08-14 02:02:20 +0300

    edits

--- Lesson_02/lesson_2_task_4.py
+++ Lesson_02/lesson_2_task_4.py
@@ -1,14 +1,15 @@
 def fizz_buzz(n):
     for a in range(1, n+1):
         if a % 3 == 0 and a % 5 == 0:
-            print ("FizzBuzz")
+            print("FizzBuzz")
         elif a % 5 == 0:
-            print ("Buzz")
+            print("Buzz")
         elif a % 3 == 0:
-            print ("Fizz")
+            print("Fizz")
         else:
             print(a)
 
+
 n = int(input("Введите число: "))
 result = fizz_buzz(n)
-print (fizz_buzz(n))
+print(fizz_buzz(n))
--- Lesson_02/lesson_2_task_5.py
+++ Lesson_02/lesson_2_task_5.py
@@ -1,15 +1,17 @@
-def month_to_season (number):
+def month_to_season(number):
     if number <= 2:
-        print ("ЗИМА")
+        print("ЗИМА")
     elif 3 <= number <= 5:
-        print ("ВЕСНА")
+        print("ВЕСНА")
     elif 6 <= number <= 8:
-        print ("ЛЕТО")
+        print("ЛЕТО")
     elif 9 <= number <= 11:
-        print ("Осень")
+        print("Осень")
     elif 13 <= number:
-        print ("В году только 12 месяцев!")
+        print("В году только 12 месяцев!")
     else:
-        print ("зима")
+        print("зима")
+
+
 number = int(input("Введите порядковый номер месяца: "))
-result = month_to_season (number)
+result = month_to_season(number)
